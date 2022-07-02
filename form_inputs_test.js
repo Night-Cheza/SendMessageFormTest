d5c4ec155b0f06c16580bc43d872bcab5de44e63
Night-Cheza <leila.kassi@outlook.com>
2022-07-02 12:22:35 -0600

    test code is completed

--- form_inputs_test.js
+++ form_inputs_test.js
@@ -2,9 +2,26 @@ Feature('required fields');
 const assert = require('assert');
 
 Scenario('Test happy path', async ({ I }) => {
-    // I.amOnPage("/contact");
-    // I.see('Submit', 'button');
-    I.testFormInputs();
+    I.testHappyPath();
     let sent = await I.grabTextFrom("h3");
     assert.equal("Message Sent (go back)", sent);
 });
+
+Scenario('Name field is required', async ({ I }) => {
+    I.testNameInput();
+    let sent = await I.grabTextFrom("h3");
+    assert.equal("Message Sent (go back)", sent);
+});
+
+Scenario('Email field is required', async ({ I }) => {
+    I.testEmailInput();
+    let sent = await I.grabTextFrom("h3");
+    assert.equal("Message Sent (go back)", sent);
+});
+
+Scenario('Website is required to be URL', async ({ I }) => {
+    I.testWebsiteURLInput();
+    let sent = await I.grabTextFrom("h3");
+    assert.equal("Message Sent (go back)", sent);
+});
+
--- helpers/input_fields_helper.js
+++ helpers/input_fields_helper.js
@@ -58,6 +58,62 @@ class InpuFieldsHelper extends Helper {
     await page.locator('text=Submit').click();
   }
 
+  async testEmailInput() {
+    const {page} = this.helpers.Playwright;
+    await page.goto("https://bluescapeqainterview.wordpress.com/contact/");
+
+    // Click input[name="g7-name"]
+    await page.locator('input[name="g7-name"]').click();
+    // Fill input[name="g7-name"]
+    await page.locator('input[name="g7-name"]').fill('name');
+
+    // Click input[name="g7-email"]
+    await page.locator('input[name="g7-email"]').click();
+    // Fill input[name="g7-email"]
+    await page.locator('input[name="g7-email"]').fill('');
+
+    // Click input[name="g7-website"]
+    await page.locator('input[name="g7-website"]').click();
+    // Fill input[name="g7-website"]
+    await page.locator('input[name="g7-website"]').fill('https://google.com');
+
+    // Click input[name="g7-date"]
+    await page.locator('input[name="g7-date"]').click();
+    // Click text=9 on the calendar
+    await page.locator('text=27').click();
+
+    // Click text=Submit
+    await page.locator('text=Submit').click();
+  }
+
+  async testWebsiteURLInput() {
+    const {page} = this.helpers.Playwright;
+    await page.goto("https://bluescapeqainterview.wordpress.com/contact/");
+
+    // Click input[name="g7-name"]
+    await page.locator('input[name="g7-name"]').click();
+    // Fill input[name="g7-name"]
+    await page.locator('input[name="g7-name"]').fill('name');
+
+    // Click input[name="g7-email"]
+    await page.locator('input[name="g7-email"]').click();
+    // Fill input[name="g7-email"]
+    await page.locator('input[name="g7-email"]').fill('abc@google.com');
+
+    // Click input[name="g7-website"]
+    await page.locator('input[name="g7-website"]').click();
+    // Fill input[name="g7-website"]
+    await page.locator('input[name="g7-website"]').fill('some website');
+
+    // Click input[name="g7-date"]
+    await page.locator('input[name="g7-date"]').click();
+    // Click text=9 on the calendar
+    await page.locator('text=27').click();
+
+    // Click text=Submit
+    await page.locator('text=Submit').click();
+  }
+
 }
 
 module.exports = InpuFieldsHelper;
